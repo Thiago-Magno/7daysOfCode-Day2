0152ad73ccac8975eebf0cba1f03806eafedfce5
Thiago-Magno <thiagomagnoom@gmail.com>
2022-04-03 19:35:17 -0300

    Exercicio 2 Resolvido

---
+++ dia2.js
@@ -0,0 +1,29 @@
+const Modal = {
+    open() {
+        //Abrir modal
+        //para abrir o modal preciso adicionar a class active ao modal
+        document.querySelector('.modal-overlay')
+            .classList.add('active')
+    },
+    close() {
+        //fechar o modal
+        //remover a class active do modal
+        document.querySelector('.modal-overlay')
+            .classList.remove('active')
+    }
+
+}
+
+function cadastra() {
+  let name = document.getElementById("name").value;
+  let age = parseInt(document.getElementById("age").value);
+  let languages = document.getElementById("language").value;
+
+  let showMessage = document.getElementById("bemVindo");
+  let welcome = "Olá " + name +" você tem " + age +
+    " anos de idade e já esta aprendendo " +
+    languages + " !";
+  showMessage.innerHTML = welcome;
+
+  console.log(welcome);
+}
